a9f63edb4f0bbdc063517957693f15afcc92cb42
miikatoi <miika.toikkanen.2@gmail.com>
2021-11-08 15:40:03 +0900

    Removed reliance on batch size and added automatic resizing.

--- image_patching/extractor.py
+++ image_patching/extractor.py
@@ -3,12 +3,20 @@ import torch
 
 
 class PatchExtractor:
+    """
+    patch_size: int                             -   Extracted patches are rectangular with side lenght patch_size
+    image_shape: Optional[List[int]] = None     -   Optionally provide image size (bs, c, h, w), otherwise it is determined on first call.
+    resize: Optional[str] = 'truncate'          -   Determine what to do with non-square or mismatching images. [interpolate | truncate]
+    """
 
     def __init__(self,
-    patch_size: int,
-    image_shape: Optional[List[int]] = None,
-    ) -> None:
+        patch_size: int,
+        image_shape: Optional[List[int]] = None,
+        resize: Optional[str] = 'truncate',
+        ) -> None:
+
         self.ps = patch_size
+        self.resize = resize
         self._input_shape_set = False
 
         if not image_shape is None:
@@ -29,13 +37,17 @@ class PatchExtractor:
         """Convert a batch of images into a batch of image patches."""
 
         if not self._input_shape_set:
+            if images.shape[-1] != images.shape[-2]:
+                raise ValueError('Trying to determine image-size from non-rectangular image. Pass image_shape in to the constructor instead.')
             self._set_image_shape(images.shape)
+
+        images = self._resize_images(images)
 
         # unfold and reshape to (np * bs, nc, ps, ps)
 
         unfolded = images.data.unfold(2, self.ps, self.ps).unfold(3, self.ps, self.ps)
-        reordered = unfolded.reshape(self.bs, self.nc, self.np, self.ps, self.ps).permute(2, 0, 1, 3, 4)
-        patches = reordered.reshape(self.np * self.bs, self.nc, self.ps, self.ps)
+        reordered = unfolded.reshape(-1, self.nc, self.np, self.ps, self.ps).permute(2, 0, 1, 3, 4)     # self.bs           -> -1
+        patches = reordered.reshape(-1, self.nc, self.ps, self.ps)                                      # self.np * self.bs -> -1
 
         return patches
 
@@ -44,6 +56,8 @@ class PatchExtractor:
         
         if not self._input_shape_set:
             raise ValueError("No input shape provided. Call split_to_patches() first.")
+
+        assert patches.shape[1:] == (self.nc, self.ps, self.ps)
 
         # Currently, fold only supports 3D-tensors, shapes should be:
         # Input: (N, C × ∏(kernel_size), L) -> (bs, nc * ps * ps, np) 
@@ -54,3 +68,15 @@ class PatchExtractor:
 
         return images
 
+    def _resize_images(self, images: torch.Tensor) -> torch.Tensor:
+        """Resize image tensor to a suitable dimension."""
+
+        if self.resize == 'truncate':
+            side = min(images.shape[2:]) - min(images.shape[2:]) % self.ps
+            images = images[:, :, :side, :side]
+        elif self.resize == 'interpolate':
+            side = min(images.shape[2:]) - min(images.shape[2:]) % self.ps
+            images = torch.nn.functional.interpolate(images, (side, side))
+        else:
+            raise NotImplementedError(f"Resize method {self.resize} not implemented")
+        return images
--- tests/test_patching.py
+++ tests/test_patching.py
@@ -27,6 +27,28 @@ class TestPatchExtractor(unittest.TestCase):
         reconstruction = self.extractor.reconstruct_image(self.extractor.split_to_patches(self.x))
         self.assertTrue((self.x == reconstruction).all())
 
+    def test_mismatch(self):
+        x1 = torch.rand((self.bs, self.nc, self.s + 20, self.s))
+        self.assertRaises(ValueError, lambda: self.extractor.split_to_patches(x1))
+
+    def test_resize_truncate(self):
+        self.extractor = PatchExtractor(patch_size=self.ps, resize='truncate')
+        self.extractor.split_to_patches(self.x)
+        x1 = torch.rand((self.bs, self.nc, self.s + 20, self.s))
+        reconstruction = self.extractor.reconstruct_image(self.extractor.split_to_patches(x1))
+        self.assertEqual(reconstruction.shape, self.x.shape)
+
+    def test_resize_interpolate(self):
+        self.extractor = PatchExtractor(patch_size=self.ps, resize='interpolate')
+        self.extractor.split_to_patches(self.x)
+        x1 = torch.rand((self.bs, self.nc, self.s + 20, self.s))
+        reconstruction = self.extractor.reconstruct_image(self.extractor.split_to_patches(x1))
+        self.assertEqual(reconstruction.shape, self.x.shape)
+
+    def test_size_undefined(self):
+        patches = torch.rand((self.bs * self.np, self.nc, self.ps, self.ps))
+        self.assertRaises(ValueError, lambda: self.extractor.reconstruct_image(patches))
+
 
 if __name__ == '__main__':
 
